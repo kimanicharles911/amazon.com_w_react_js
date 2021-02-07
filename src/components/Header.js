4f33776e6aedc230e2133f4c3d7a3bff96809fea
kimanicharles911 <kimanicharles911@gmail.com>
2021-02-07 23:04:56 +0300

    navbar in progress 42:40

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,41 @@
-import React from 'react'
-import "./Header.css"
+import React from 'react';
+import "./Header.css";
+import logo from "../amazon_PNG11.png";
 
 function Header() {
   return (
     <div className="header">
-      <img className="header_logo" src="../amazon_PNG11.png" alt=""/>
+      <img className="header__logo" src={logo} alt=""/>
       <div className="header__search">
         <input type="text" className="header__searchInput"/>
       </div>
       <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">
+            Hello Guest
+          </span>
+          <span className="header__optionLineTwo">
+            Sign In
+          </span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__optionLineOne">
+            Returns
+          </span>
+          <span className="header__optionLineTwo">
+            & Orders
+          </span>          
+        </div>
         
+        <div className="header__option">
+          <span className="header__optionLineOne">
+            Your
+          </span>
+          <span className="header__optionLineTwo">
+            Prime
+          </span>          
+        </div>
       </div>
     </div>
   )
